b2da1235ea66547c5a7b2edb950db7bfd6ce4367
Diyorbek <diyoreshmurodov99@gmail.com>
2026-09-10 08:29:16 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -147,11 +147,12 @@ CSRF_COOKIE_HTTPONLY = False
 SESSION_COOKIE_SAMESITE = 'Lax'
 CSRF_COOKIE_SAMESITE = 'Lax'
 BOT_TOKEN = config('BOT_TOKEN')
-ALLOWED_HOSTS = [
-    'zippy-upon-unscathed.ngrok-free.dev',
-    '127.0.0.1',
-    'localhost',
-]
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = [
+#     'zippy-upon-unscathed.ngrok-free.dev',
+#     '127.0.0.1',
+#     'localhost',
+# ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://zippy-upon-unscathed.ngrok-free.dev',
